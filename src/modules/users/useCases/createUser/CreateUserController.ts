7461e449630422dcaadd16bb33ab55f348e7c2f1
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-18 12:12:47 -0300

    Feat: add create user useCase and controller logic

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -1,12 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { Response, Request } from "express";
 
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+  constructor(private createUserUseCase: CreateUserUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { name, email } = request.body;
+
+    this.createUserUseCase.execute({ email, name });
+
+    return response.status(201).send();
   }
 }
 
